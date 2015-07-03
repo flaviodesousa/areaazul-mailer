703e9fefae0038de7bced99517dd3cb2819961d3
Flávio de Sousa <flavio.desousa@ipoque.com>
2015-07-03 15:11:56 +0200

    jscs + jshint

--- enviar.js
+++ enviar.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var nodemailer = require("./nodemailer.js");
+var nodemailer = require('./nodemailer.js');
 
-exports.emailer = function(message){
-	nodemailer.enviarEmail(message, function(result, error) {
-	    if (result) {
-	        console.log("Email enviado com sucesso!!!");
-	        console.log(result);
-	    }
-	    if(error){
-	    	 	console.log("Problemas ao enviar o email!!!");
-	    		console.log(error);
-	    }
-	});
+exports.emailer = function(message) {
+  nodemailer.enviarEmail(message, function(result, error) {
+    if (result) {
+      console.log('Email enviado com sucesso!!!');
+      console.log(result);
+    }
+    if (error) {
+      console.log('Problemas ao enviar o email!!!');
+      console.log(error);
+    }
+  });
 };
--- nodemailer.js
+++ nodemailer.js
@@ -1,34 +1,36 @@
 'use strict';
 
-var nodemailer = require("nodemailer");
+var nodemailer = require('nodemailer');
 
-if (! nodemailer.serviceConfig) {
-    var emailUser = process.env.AREAAZUL_EMAIL_USER || 'cadastro@areaazul.org';
-    var emailPassword = process.env.AREAAZUL_EMAIL_PASSWORD || 'MnyAgaqNZAIKWuqLFzSt-w';
-    nodemailer.serviceConfig = nodemailer.createTransport(
-        "SMTP",
-        {
-            host: 'smtp.mandrillapp.com',
-            auth: {
-                user: emailUser,
-                pass: emailPassword
-            }
-        }
-    );
+if (!nodemailer.serviceConfig) {
+  var emailUser = process.env.AREAAZUL_EMAIL_USER ||
+    'cadastro@areaazul.org';
+  var emailPassword = process.env.AREAAZUL_EMAIL_PASSWORD ||
+    'MnyAgaqNZAIKWuqLFzSt-w';
+  nodemailer.serviceConfig = nodemailer.createTransport(
+    'SMTP',
+    {
+      host: 'smtp.mandrillapp.com',
+      auth: {
+        user: emailUser,
+        pass: emailPassword,
+      },
+    }
+  );
 }
 
 exports.enviarEmail = function(message, func) {
-    nodemailer.serviceConfig.sendMail({ //email options
-        from: message.from, // E-mail de Origem
-        to: message.to,
-        cc: message.cc,
-        subject: message.subject, // subject
-        html: message.html
-    }, function(error, response) { //callback
-        if (error) {
-            return func(error);
-        } else {
-            return func(response);
-        }
-    });
+  nodemailer.serviceConfig.sendMail({ // Email options
+    from: message.from, // E-mail de Origem
+    to: message.to,
+    cc: message.cc,
+    subject: message.subject,
+    html: message.html,
+  }, function(error, response) {
+    if (error) {
+      return func(error);
+    } else {
+      return func(response);
+    }
+  });
 };
